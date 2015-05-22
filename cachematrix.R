179d7079309ac6e39f6e0837ac6eebb336c5cd5e
domran <dylanomran2@gmail.com>
2015-05-22 21:02:18 +0100

    Solutions added for both funcions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions that either retrieve a matrix inverse where it exists 
+## or calculate and retrieve it where it doesn't (thus reducing processing time)
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Function 1: Create a list vector of callable functions that can be used by Function 2
+makeCacheMatrix <- function(x = matrix())
+{
+  m <- NULL
+  set <- function(y){    ## assign values to the parent frame variables 'x' and 'm' 
+    x <<- y
+    m <<- NULL
+  }  
+  get <- function(){    ## retrieves the original matrix
+    x
+  }  
+  setinv <- function(matinv){    ## assigns the inverse matrix value to the parent frame variable
+    m <<- matinv
+  }   
+  getinv <- function(){    ## retrieves the inverse matrix value
+    m  
+  } 
+  ## finally, create the list of the functions created above
+  list(
+    set = set,
+    get = get,
+    setinv = setinv,
+    getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
+## Function 2: calculate / retrieve the matrix inverse using the functions created above
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)    ## Matrix inversion
+  x$setinv(m)
+  m
 }
